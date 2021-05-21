0b55dcc1096b7b833229e3ad0335833408501a4e
Derek Feehrer <derekfeehrer@gmail.com>
2021-05-20 20:05:53 -0400

    Update build-ffmpeg.sh

--- wasm/build-scripts/build-ffmpeg.sh
+++ wasm/build-scripts/build-ffmpeg.sh
@@ -13,8 +13,8 @@ FLAGS=(
   fftools/ffmpeg_opt.c fftools/ffmpeg_filter.c fftools/ffmpeg_hw.c fftools/cmdutils.c fftools/ffmpeg.c
   -o wasm/dist/ffmpeg-core.js
   -s USE_SDL=2                                  # use SDL2
-  -s USE_PTHREADS=0                             # enable pthreads support
-  -s PROXY_TO_PTHREAD=0                         # detach main() from browser/UI main thread
+  -s USE_PTHREADS=1                             # enable pthreads support
+  -s PROXY_TO_PTHREAD=1                         # detach main() from browser/UI main thread
   -s INVOKE_RUN=0                               # not to run the main() in the beginning
   -s EXIT_RUNTIME=1                             # exit runtime after execution
   -s MODULARIZE=1                               # use modularized version to be more flexible
